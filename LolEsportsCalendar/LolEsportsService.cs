02abbc0d360b8d814db7102c35fc36dc73ab2ab9
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2022-01-19 19:49:24 +0100

    Moved lolesport to seperated service

--- LolEsportsCalendar/LolEsportsService.cs
+++ LolEsportsCalendar/LolEsportsService.cs
@@ -1,10 +1,145 @@
-﻿using System;
+﻿using Google.Apis.Calendar.v3.Data;
+using LolEsportsApiClient;
+using LolEsportsApiClient.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LolEsportsCalendar
 {
 	public class LolEsportsService
 	{
+		private readonly List<string> selectedLeagues = new List<string>
+		{
+			"LEC",
+			"European Masters",
+			"Worlds"
+		};
+		private readonly Dictionary<string, string> leagueLookup = new Dictionary<string, string>();
+
+		public LolEsportsClient _lolEsportsClient = new LolEsportsClient();
+		public GoogleCalendarService _googleCalendarService = new GoogleCalendarService();
+
+		public LolEsportsService()
+		{
+			BuildLeagueLookup().GetAwaiter().GetResult();
+		}
+
+		public async Task<List<League>> GetLeagues()
+		{
+			List<League> leagues = null;
+
+			try
+			{
+				leagues = await _lolEsportsClient.GetLeagues();
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+			}
+
+			return leagues;
+		}
+
+		public async Task BuildLeagueLookup()
+		{
+			var leagues = await GetLeagues();
+
+			if (leagues != null)
+			{
+				foreach (var league in leagues)
+				{
+					leagueLookup.Add(league.Name, league.Id);
+				}
+			}
+		}
+
+		public async Task ImportMissingCalendars()
+		{
+			try
+			{
+				var leagues = await GetLeagues();
+
+				foreach (var league in leagues)
+				{
+					// Skip unselected leagues
+					if (!selectedLeagues.Contains(league.Name))
+					{
+						continue;
+					}
+
+					// Check if calendar exists for league
+					bool exists = _googleCalendarService.CalendarExists(league.Name);
+
+					if (!exists)
+					{
+						_googleCalendarService.InsertLeagueAsCalendar(league);
+					}
+				}
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+				throw;
+			}
+		}
+
+		public async Task ImportEventsForAllCalendars()
+		{
+			try
+			{
+				var leauges = await GetLeagues();
+
+				foreach (League leauge in leauges)
+				{
+					// Skip unselected leagues
+					if (!selectedLeagues.Contains(leauge.Name))
+					{
+						continue;
+					}
+
+					ImportEventsForLeague(leauge.Name).GetAwaiter().GetResult();
+				}
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+			}
+		}
+
+		public async Task ImportEventsForLeague(string leagueName)
+		{
+			try
+			{
+				// Get calendarId
+				string calendarId = _googleCalendarService.FindCalendarId(leagueName);
+
+				// Get leagueId
+				string leagueId = leagueLookup[leagueName];
+
+				// Get events of league
+				var events = await _lolEsportsClient.GetScheduleByLeague(leagueId);
+
+				foreach (EsportEvent esportEvent in events)
+				{
+					// Skip unselected leagues
+					if (!selectedLeagues.Contains(esportEvent.League.Name))
+					{
+						continue;
+					}
+
+					// Convert LeagueEvent to GoogleEvent
+					Event @event = _googleCalendarService.ConvertEsportEventToGoogleEvent(esportEvent);
+
+					// Insert or Update GoogleEvent
+					_googleCalendarService.InsertOrUpdateEvent(@event, calendarId, @event.Id);
+				}
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+			}
+		}
 	}
 }
--- LolEsportsCalendar/Program.cs
+++ LolEsportsCalendar/Program.cs
@@ -12,21 +12,11 @@ namespace LolEsportsCalendar
 {
 	class Program
 	{
-		static readonly Dictionary<string, string> leagueLookup = new Dictionary<string, string>();
-		static LolEsportsClient _lolEsportsClient = new LolEsportsClient();
 		static GoogleCalendarService _googleCalendarService = new GoogleCalendarService();
-
-		static readonly List<string> selectedLeagues = new List<string>
-		{
-			"LEC",
-			"European Masters",
-			"Worlds"
-		};
+		static LolEsportsService _lolEsportsService = new LolEsportsService();
 
 		static void Main(string[] args)
 		{
-			BuildLeagueLookup().GetAwaiter().GetResult();
-
 			// Google Calendar API
 
 			// Clear calendar
@@ -63,130 +53,11 @@ namespace LolEsportsCalendar
 			var newEvent = _googleCalendarService.InsertOrUpdateEvent(_event, calendarId, esportEvent.Match.Id);
 			Console.WriteLine(newEvent.Summary);
 
-			// SyncCalendars().GetAwaiter().GetResult();
-
-			// SyncScheduledEvents
-			// SyncEventsForLeague().GetAwaiter().GetResult();
-
 			// Lolesports API
-			// RunAsync().GetAwaiter().GetResult();
+			// _lolEsportsService.ImportMissingCalendars().GetAwaiter().GetResult();
+			// _lolEsportsService.ImportEventsForAllCalendars().GetAwaiter().GetResult();
+			// _lolEsportsService.ImportEventsForLeague("LEC").GetAwaiter().GetResult();
 
-			Console.ReadLine();
-		}
-
-		static async Task SyncCalendars()
-		{
-			try
-			{
-				var leagues = await _lolEsportsClient.GetLeagues();
-
-				foreach (var league in leagues)
-				{
-					// Skip unselected leagues
-					if (!selectedLeagues.Contains(league.Name)) {
-						continue;
-					}
-
-					// Check if calendar exists for league
-					bool exists = _googleCalendarService.CalendarExists(league.Name);
-
-					if (!exists) {
-						_googleCalendarService.InsertLeagueAsCalendar(league);
-					}
-				}
-			}
-			catch (Exception exception)
-			{
-				Console.WriteLine(exception.Message);
-				throw;
-			}
-		}
-
-		static async Task BuildLeagueLookup()
-		{
-			try
-			{
-				var leagues = await _lolEsportsClient.GetLeagues();
-
-				foreach (var league in leagues)
-				{
-					leagueLookup.Add(league.Name, league.Id);
-				}
-			}
-			catch (Exception exception)
-			{
-				Console.WriteLine(exception.Message);
-				throw;
-			}
-		}
-
-		static async Task SyncEventsForLeague()
-		{
-			try
-			{
-				string calendarId = _googleCalendarService.FindCalendarId("LEC");
-				string leagueId = leagueLookup["LEC"];
-				var events = await _lolEsportsClient.GetScheduleByLeague(leagueId);
-
-				foreach (EsportEvent esportEvent in events)
-				{
-					// Skip unselected leagues
-					if (!selectedLeagues.Contains(esportEvent.League.Name))
-					{
-						continue;
-					}
-					Event @event = _googleCalendarService.ConvertEsportEventToGoogleEvent(esportEvent);
-					_googleCalendarService.InsertEvent(@event, calendarId);
-				}
-			}
-			catch (Exception exception)
-			{
-				Console.WriteLine(exception.Message);
-				throw;
-			}
-		}
-
-		static async Task RunAsync()
-		{
-			try
-			{
-				// 1 get leagues
-				/*
-				Console.WriteLine("1 Get Leagues:");
-				var leagues = await lolEsportsClient.GetLeagues();
-
-				foreach (var league in leagues)
-				{
-					Console.WriteLine(league.Name);
-				}
-				*/
-
-				// 2 get schedules
-				/*
-				Console.WriteLine("2 Get Schedule:");
-				var events = await lolEsportsClient.GetSchedule();
-
-				foreach (var _event in events)
-				{
-					Console.WriteLine(_event.League.Name);
-				}
-				*/
-
-				// 3 get schedule of league
-				var leaugeId = "98767991302996019";
-				Console.WriteLine("2 Get Schedule of league:");
-				var events = await _lolEsportsClient.GetScheduleByLeague(leaugeId);
-
-				foreach (var _event in events)
-				{
-					Console.WriteLine(_event.League.Name);
-				}
-			}
-			catch (Exception exception)
-			{
-				Console.WriteLine(exception.Message);
-				throw;
-			}
 			Console.ReadLine();
 		}
 	}
