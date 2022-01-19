2574be7d49962b341e9e35268767fc78c11465c9
ArnoutPullen <ArnoutPullen@users.noreply.github.com>
2022-01-19 19:20:01 +0100

    Moved google calendar to seperated service

--- LolEsportsCalendar/GoogleCalendarService.cs
+++ LolEsportsCalendar/GoogleCalendarService.cs
@@ -1,6 +1,7 @@
 ﻿using Google.Apis.Calendar.v3.Data;
 using GoogleCalendarApiClient;
 using GoogleCalendarApiClient.Services;
+using LolEsportsApiClient.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +10,182 @@ namespace LolEsportsCalendar
 {
 	public class GoogleCalendarService
 	{
+		private readonly Dictionary<string, string> calendarLookup = new Dictionary<string, string>();
+		private CalendarList calendars;
+		readonly List<string> selectedLeagues = new List<string>
+		{
+			"LEC",
+			"European Masters",
+			"Worlds"
+		};
+
+		private GoogleCalendarClient _googleCalendarClient = new GoogleCalendarClient();
+		private CalendarsService _calendarsService;
+		private CalendarListService _calendarListService;
+		private EventsService _eventsService;
+
+		public GoogleCalendarService()
+		{
+			_calendarsService = new CalendarsService(_googleCalendarClient.calendarService);
+			_calendarListService = new CalendarListService(_googleCalendarClient.calendarService);
+			_eventsService = new EventsService(_googleCalendarClient.calendarService);
+
+			BuildCalendarLookup();
+		}
+
+		public void BuildCalendarLookup()
+		{
+			// View
+			CalendarList calendarList = _calendarListService.ViewCalendarList();
+			calendars = calendarList;
+
+			// Add calendars to lookup
+			foreach (var c in calendarList.Items)
+			{
+				// calendarLookup.Add(c.Id, c.Summary);
+				calendarLookup.Add(c.Summary, c.Id);
+			}
+		}
+
+		public string FindCalendarId(string leagueName)
+		{
+			return calendarLookup[leagueName];
+		}
+
+		public bool CalendarExists(string key)
+		{
+			return calendarLookup.ContainsKey(key);
+		}
+
+		public void ClearCalendar(string calendarId)
+		{
+			// get events
+			Events events = _eventsService.List(calendarId);
+			Console.WriteLine("Deleting {0} events", events.Items.Count);
+
+			// delete events
+			foreach (Event e in events.Items)
+			{
+				string deleted = _eventsService.Delete(calendarId, e.Id);
+
+				if (deleted == "")
+				{
+					Console.WriteLine("Deleted {0} from calendar {1}", e.Summary, calendarId);
+				}
+				else
+				{
+					Console.WriteLine("Error while deleting {0} from calendar {1}", e.Summary, calendarId);
+				}
+			}
+		}
+
+		public Calendar InsertLeagueAsCalendar(League league)
+		{
+			Calendar calendar = new Calendar
+			{
+				Summary = league.Name,
+				Description = league.Name + " / " + league.Region,
+				// ETag = "Test",
+				// Kind = "Test",
+				// Location = "Test",
+				// TimeZone = "Europe/Amsterdam"
+			};
+
+			Calendar newCalendar = _calendarsService.InsertCalendar(calendar);
+
+			if (calendar != null)
+			{
+				calendarLookup.Add(calendar.Summary, calendar.Id);
+				Console.WriteLine("Created calendar {0}", calendar.Summary);
+			}
+			else
+			{
+				Console.WriteLine("Error while creating calendar");
+			}
+
+			return newCalendar;
+		}
+
+		public Event GetEvent(string calendarId, string eventId)
+		{
+			Event newEvent = null;
+			try
+			{
+				newEvent = _eventsService.Get(calendarId, eventId);
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+			}
+
+			return newEvent;
+		}
+
+		public Event InsertOrUpdateEvent(Event @event, string calendarId, string eventId)
+		{
+			Event changedEvent;
+
+			// Check if event already exists
+			var existingEvent = GetEvent(calendarId, eventId);
+
+			if (existingEvent == null)
+			{
+				changedEvent = InsertEvent(@event, calendarId);
+			}
+			else
+			{
+				changedEvent = UpdateEvent(@event, calendarId, eventId);
+			}
+
+			return changedEvent;
+		}
+
+		public Event InsertEvent(Event @event, string calendarId)
+		{
+			Event newEvent = _eventsService.Insert(@event, calendarId);
+
+			if (newEvent != null)
+			{
+				Console.WriteLine("Created event {0}", newEvent.Summary);
+			}
+			else
+			{
+				Console.WriteLine("Error while creating event in calendar, {0}", calendarId);
+			}
+
+			return newEvent;
+		}
+
+		public Event UpdateEvent(Event @event, string calendarId, string eventId)
+		{
+			Event updatedEvent = _eventsService.Update(@event, calendarId, eventId);
+
+			if (updatedEvent != null)
+			{
+				Console.WriteLine("Updated event {0}", updatedEvent.Summary);
+			}
+			else
+			{
+				Console.WriteLine("Error while updating event in calendar, {0}", calendarId);
+			}
+
+			return updatedEvent;
+		}
+
+		public Event ConvertEsportEventToGoogleEvent(EsportEvent esportEvent)
+		{
+			// Convert EsportEvent to Event
+			Event @event = new Event()
+			{
+				Id = esportEvent.Match.Id,
+				// ICalUID = esportEvent.Match.Id,
+				Start = new EventDateTime { DateTime = esportEvent.StartTime.UtcDateTime },
+				End = new EventDateTime { DateTime = esportEvent.StartTime.UtcDateTime },
+				// Summary = esportEvent.Match.Teams[0].Code + " vs " + esportEvent.Match.Teams[1].Code
+				Summary = "Test"
+			};
+
+			return @event;
+		}
 	}
 }
--- LolEsportsCalendar/Program.cs
+++ LolEsportsCalendar/Program.cs
@@ -12,15 +12,9 @@ namespace LolEsportsCalendar
 {
 	class Program
 	{
-		static readonly Dictionary<string, string> calendarLookup = new Dictionary<string, string>();
 		static readonly Dictionary<string, string> leagueLookup = new Dictionary<string, string>();
-		static CalendarList calendars;
-
-		static GoogleCalendarClient _googleCalendarClient = new GoogleCalendarClient();
-		static CalendarsService _calendarsService;
-		static CalendarListService _calendarListService;
-		static EventsService _eventsService;
 		static LolEsportsClient _lolEsportsClient = new LolEsportsClient();
+		static GoogleCalendarService _googleCalendarService = new GoogleCalendarService();
 
 		static readonly List<string> selectedLeagues = new List<string>
 		{
@@ -31,17 +25,12 @@ namespace LolEsportsCalendar
 
 		static void Main(string[] args)
 		{
-			_calendarsService = new CalendarsService(_googleCalendarClient.calendarService);
-			_calendarListService = new CalendarListService(_googleCalendarClient.calendarService);
-			_eventsService = new EventsService(_googleCalendarClient.calendarService);
-
-			// Google Calendar API
-			BuildCalendarLookup();
-
 			BuildLeagueLookup().GetAwaiter().GetResult();
 
+			// Google Calendar API
+
 			// Clear calendar
-			string calendarId = calendarLookup["LEC"];
+			string calendarId = _googleCalendarService.FindCalendarId("LEC");
 			// ClearCalendar(calendarId);
 
 			// test duplicates
@@ -67,11 +56,11 @@ namespace LolEsportsCalendar
 				},
 				StartTime = DateTimeOffset.Now
 			};
-			 // var newEvent = _eventsService.Insert(@event, calendarId);
-			 // Convert EsportEvent to Event
-			 Event _event = ConvertEsportEventToGoogleEvent(esportEvent);
+			// var newEvent = _eventsService.Insert(@event, calendarId);
+			// Convert EsportEvent to Event
+			 Event _event = _googleCalendarService.ConvertEsportEventToGoogleEvent(esportEvent);
 
-			var newEvent = InsertOrUpdateEvent(_event, calendarId, esportEvent.Match.Id);
+			var newEvent = _googleCalendarService.InsertOrUpdateEvent(_event, calendarId, esportEvent.Match.Id);
 			Console.WriteLine(newEvent.Summary);
 
 			// SyncCalendars().GetAwaiter().GetResult();
@@ -82,54 +71,7 @@ namespace LolEsportsCalendar
 			// Lolesports API
 			// RunAsync().GetAwaiter().GetResult();
 
-			// Test();
 			Console.ReadLine();
-		}
-
-		static void Test()
-		{
-			string calendarId = calendarLookup["LEC"];
-			Event @event = _eventsService.Get(calendarId, "106269680921808997");
-			Console.WriteLine(@event);
-		}
-
-		static void BuildCalendarLookup()
-		{
-			// View
-			CalendarList calendarList = _calendarListService.ViewCalendarList();
-			calendars = calendarList;
-
-			// Add calendars to lookup
-			foreach (var c in calendarList.Items)
-			{
-				// calendarLookup.Add(c.Id, c.Summary);
-				calendarLookup.Add(c.Summary, c.Id);
-			}
-		}
-
-		static Calendar InsertLeagueAsCalendar(League league)
-		{
-			Calendar calendar = new Calendar
-			{
-				Summary = league.Name,
-				Description = league.Name + " / " + league.Region,
-				// ETag = "Test",
-				// Kind = "Test",
-				// Location = "Test",
-				// TimeZone = "Europe/Amsterdam"
-			};
-
-			Calendar newCalendar = _calendarsService.InsertCalendar(calendar);
-
-			if (calendar != null)
-			{
-				calendarLookup.Add(calendar.Summary, calendar.Id);
-				Console.WriteLine("Created calendar {0}", calendar.Summary);
-			} else {
-				Console.WriteLine("Error while creating calendar");
-			}
-
-			return newCalendar;
 		}
 
 		static async Task SyncCalendars()
@@ -146,10 +88,10 @@ namespace LolEsportsCalendar
 					}
 
 					// Check if calendar exists for league
-					bool exists = calendarLookup.ContainsKey(league.Name);
+					bool exists = _googleCalendarService.CalendarExists(league.Name);
 
 					if (!exists) {
-						InsertLeagueAsCalendar(league);
+						_googleCalendarService.InsertLeagueAsCalendar(league);
 					}
 				}
 			}
@@ -178,112 +120,11 @@ namespace LolEsportsCalendar
 			}
 		}
 
-		static Event GetEvent(string calendarId, string eventId)
-		{
-			Event newEvent = null;
-			try
-			{
-				newEvent = _eventsService.Get(calendarId, eventId);
-			}
-			catch (Exception exception)
-			{
-				Console.WriteLine(exception.Message);
-			}
-
-			return newEvent;
-		}
-
-		static Event InsertOrUpdateEvent(Event @event, string calendarId, string eventId)
-		{
-			Event changedEvent;
-
-			// Check if event already exists
-			var existingEvent = GetEvent(calendarId, eventId);
-
-			if (existingEvent == null)
-			{
-				changedEvent = InsertEvent(@event, calendarId);
-			}
-			else
-			{
-				changedEvent = UpdateEvent(@event, calendarId, eventId);
-			}
-
-			return changedEvent;
-		}
-
-		static Event InsertEvent(Event @event, string calendarId)
-		{
-			Event newEvent = _eventsService.Insert(@event, calendarId);
-
-			if (newEvent != null)
-			{
-				Console.WriteLine("Created event {0}", newEvent.Summary);
-			}
-			else
-			{
-				Console.WriteLine("Error while creating event in calendar, {0}", calendarId);
-			}
-
-			return newEvent;
-		}
-
-		static Event UpdateEvent(Event @event, string calendarId, string eventId)
-		{
-			Event updatedEvent = _eventsService.Update(@event, calendarId, eventId);
-
-			if (updatedEvent != null)
-			{
-				Console.WriteLine("Updated event {0}", updatedEvent.Summary);
-			}
-			else
-			{
-				Console.WriteLine("Error while updating event in calendar, {0}", calendarId);
-			}
-
-			return updatedEvent;
-		}
-
-		static Event ConvertEsportEventToGoogleEvent(EsportEvent esportEvent)
-		{
-			// Convert EsportEvent to Event
-			Event @event = new Event()
-			{
-				Id = esportEvent.Match.Id,
-				// ICalUID = esportEvent.Match.Id,
-				Start = new EventDateTime { DateTime = esportEvent.StartTime.UtcDateTime },
-				End = new EventDateTime { DateTime = esportEvent.StartTime.UtcDateTime },
-				// Summary = esportEvent.Match.Teams[0].Code + " vs " + esportEvent.Match.Teams[1].Code
-				Summary = "Test"
-			};
-
-			return @event;
-		}
-
-		static void ClearCalendar(string calendarId)
-		{
-			// get events
-			Events events = _eventsService.List(calendarId);
-			Console.WriteLine("Deleting {0} events", events.Items.Count);
-
-			// delete events
-			foreach (Event e in events.Items)
-			{
-				string deleted = _eventsService.Delete(calendarId, e.Id);
-
-				if (deleted == "") {
-					Console.WriteLine("Deleted {0} from calendar {1}", e.Summary, calendarId);
-				} else {
-					Console.WriteLine("Error while deleting {0} from calendar {1}", e.Summary, calendarId);
-				}
-			}
-		}
-
 		static async Task SyncEventsForLeague()
 		{
 			try
 			{
-				string calendarId = calendarLookup["LEC"];
+				string calendarId = _googleCalendarService.FindCalendarId("LEC");
 				string leagueId = leagueLookup["LEC"];
 				var events = await _lolEsportsClient.GetScheduleByLeague(leagueId);
 
@@ -294,8 +135,8 @@ namespace LolEsportsCalendar
 					{
 						continue;
 					}
-					Event @event = ConvertEsportEventToGoogleEvent(esportEvent);
-					InsertEvent(@event, calendarId);
+					Event @event = _googleCalendarService.ConvertEsportEventToGoogleEvent(esportEvent);
+					_googleCalendarService.InsertEvent(@event, calendarId);
 				}
 			}
 			catch (Exception exception)
